d8d248d40074748aa2e897291012de095e188d5a
couleurm <couleur@riseup.net>
2021-08-12 12:16:51 +0200

    fix CLInstall, now uses batch to launch CL

--- CLinstall.ps1
+++ CLinstall.ps1
@@ -1,23 +1,24 @@
-﻿# Remove any previous CustomizableLauncher instances to prevent errors.
+﻿if (Test-Path "$env:windir\system32\WindowsPowerShell\v1.0\powershell.exe") {
+# Remove any previous CustomizableLauncher instances to prevent errors.
 Remove-Item "$env:localappdata\Microsoft\WindowsApps\CustomizableLauncher.ps1" -ErrorAction SilentlyContinue
 Remove-Item "$env:localappdata\Microsoft\WindowsApps\CL.lnk" -ErrorAction SilentlyContinue
+Remove-Item "$env:localappdata\Microsoft\WindowsApps\CL.bat" -ErrorAction SilentlyContinue
 #Downloads the customizable launcher
 $source = "https://github.com/couleur-tweak-tips/utils/raw/main/CustomizableLauncher.ps1"
 $destination = "$env:localappdata\Microsoft\WindowsApps\CustomizableLauncher.ps1"
 $webClient = [System.Net.WebClient]::new()
 $webClient.DownloadFile($source, $destination)
 #Makes the shortcut
-$ShortCutName= "CL"
-$ShortCutArguments= "iex $env:localappdata\Microsoft\WindowsApps\CustomizableLauncher.ps1"
-$ShortcutTargetPath= "powershell"
-$ShortCutPath="$env:localappdata\Microsoft\WindowsApps\"
-$WScriptShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WScriptShell.CreateShortcut("$ShortCutPath$ShortCutName.lnk")
-$Shortcut.TargetPath = "$ShortcutTargetPath"
-$Shortcut.Arguments = "$ShortCutArguments"
-$Shortcut.Save()
-# Launches the shortcut (powershell needs to specify the .lnk but via Run you don't)
-powershell iex cl.lnk
+"powershell %localappdata%\Microsoft\WindowsApps\CustomizableLauncher.ps1 %1 %2 %3 %4 %5 %6 %7 %8 %9" | Set-Content $env:localappdata\Microsoft\WindowsApps\CL.bat
+# Launches the batch (powershell needs to specify the .bat but via Run you don't)
+start cl.bat
+exit
+}else{
+Write-Host "Powershell hasn't been detected on your system, please re-enable it in optional features"
+timeout 3
+start-process optionalfeatures
+exit
+}
 
 
 
