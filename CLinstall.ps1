6a2b8b903dc430640590f508b0360831cbb02a9e
couleurm <couleur@riseup.net>
2021-08-13 12:48:54 +0200

    fix executionpolicy

--- CLinstall.ps1
+++ CLinstall.ps1
@@ -1,4 +1,6 @@
 ﻿if (Test-Path "$env:windir\system32\WindowsPowerShell\v1.0\powershell.exe") {
+#Allow execution of PowerShell scripts
+Set-ExecutionPolicy Unrestricted
 # Remove any previous CustomizableLauncher instances to prevent errors.
 Remove-Item "$env:localappdata\Microsoft\WindowsApps\CustomizableLauncher.ps1" -ErrorAction SilentlyContinue
 Remove-Item "$env:localappdata\Microsoft\WindowsApps\CL.lnk" -ErrorAction SilentlyContinue
@@ -11,7 +13,7 @@ $webClient.DownloadFile($source, $destination)
 #Makes the shortcut
 "powershell %localappdata%\Microsoft\WindowsApps\CustomizableLauncher.ps1 %1 %2 %3 %4 %5 %6 %7 %8 %9" | Set-Content $env:localappdata\Microsoft\WindowsApps\CL.bat
 # Launches the batch (powershell needs to specify the .bat but via Run you don't)
-start cl.bat
+Start-Process cl.bat
 exit
 }else{
 Write-Host "Powershell hasn't been detected on your system, please re-enable it in optional features"
