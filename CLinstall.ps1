0cd0d389844a79c1ae69ef603c8f0fbad7bb8d2e
couleurm <couleur@riseup.net>
2021-08-14 11:30:43 +0200

    ExecutionPolicy testing

--- CLinstall.ps1
+++ CLinstall.ps1
@@ -1,17 +1,15 @@
 ﻿if (Test-Path "$env:windir\system32\WindowsPowerShell\v1.0\powershell.exe") {
-#Allow execution of PowerShell scripts
-Set-ExecutionPolicy Unrestricted
-# Remove any previous CustomizableLauncher instances to prevent errors.
-Remove-Item "$env:localappdata\Microsoft\WindowsApps\CustomizableLauncher.ps1" -ErrorAction SilentlyContinue
-Remove-Item "$env:localappdata\Microsoft\WindowsApps\CL.lnk" -ErrorAction SilentlyContinue
-Remove-Item "$env:localappdata\Microsoft\WindowsApps\CL.bat" -ErrorAction SilentlyContinue
+# Remove any previous CustomizableLauncher installs to prevent errors.
+Remove-Item "$env:localappdata\Microsoft\WindowsApps\CustomizableLauncher.ps1"-Force -ErrorAction SilentlyContinue
+Remove-Item "$env:localappdata\Microsoft\WindowsApps\CL.lnk"-Force -ErrorAction SilentlyContinue
+Remove-Item "$env:localappdata\Microsoft\WindowsApps\CL.bat"-Force -ErrorAction SilentlyContinue
 #Downloads the customizable launcher
 $source = "https://github.com/couleur-tweak-tips/utils/raw/main/CustomizableLauncher.ps1"
 $destination = "$env:localappdata\Microsoft\WindowsApps\CustomizableLauncher.ps1"
 $webClient = [System.Net.WebClient]::new()
 $webClient.DownloadFile($source, $destination)
 #Makes the shortcut
-"powershell %localappdata%\Microsoft\WindowsApps\CustomizableLauncher.ps1 %1 %2 %3 %4 %5 %6 %7 %8 %9" | Set-Content $env:localappdata\Microsoft\WindowsApps\CL.bat
+"powershell Set-ExecutionPolicy Unrestricted -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex iex %localappdata%\Microsoft\WindowsApps\CustomizableLauncher.ps1 %1 %2 %3 %4 %5 %6 %7 %8 %9" | Set-Content $env:localappdata\Microsoft\WindowsApps\CL.bat
 # Launches the batch (powershell needs to specify the .bat but via Run you don't)
 Start-Process cl.bat
 exit
