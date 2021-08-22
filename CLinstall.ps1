43b1d2d209ad7c580326b7225e3dbf7fa2ae3a94
couleurm <couleur@riseup.net>
2021-08-22 16:21:20 +0200

    CLInstall now explains what steps it's going through

--- CLinstall.ps1
+++ CLinstall.ps1
@@ -1,26 +1,19 @@
-if (Test-Path "$env:windir\system32\WindowsPowerShell\v1.0\powershell.exe") {
-# Remove any previous CustomizableLauncher installs to prevent errors.
-Remove-Item "$env:localappdata\Microsoft\WindowsApps\CustomizableLauncher.ps1"-Force -ErrorAction SilentlyContinue
-Remove-Item "$env:localappdata\Microsoft\WindowsApps\CL.lnk"-Force -ErrorAction SilentlyContinue
-Remove-Item "$env:localappdata\Microsoft\WindowsApps\CL.bat"-Force -ErrorAction SilentlyContinue
-#Downloads the customizable launcher
+Write-Output "Removing any previous CustomizableLauncher installs to prevent errors..."
+$WindowsAppsDir = "$env:localappdata\Microsoft\WindowsApps"
+Remove-Item "$WindowsAppsDir\CustomizableLauncher.ps1"-Force -ErrorAction SilentlyContinue
+Remove-Item "$WindowsAppsDir\CL.lnk"-Force -ErrorAction SilentlyContinue
+Remove-Item "$WindowsAppsDir\CL.bat"-Force -ErrorAction SilentlyContinue
+Write-Output "Downloading the CL.."
 $source = "https://github.com/couleur-tweak-tips/utils/raw/main/CustomizableLauncher.ps1"
-$destination = "$env:localappdata\Microsoft\WindowsApps\CustomizableLauncher.ps1"
+$destination = "$WindowsAppsDir\CustomizableLauncher.ps1"
 $webClient = [System.Net.WebClient]::new()
 $webClient.DownloadFile($source, $destination)
-#Makes the shortcut
+Write-Output "Setting up the shortcut.."
 "title CL.bat & mode con lines=1 cols=30
 powershell Set-ExecutionPolicy Unrestricted -Scope Process -Force;%localappdata%\Microsoft\WindowsApps\CustomizableLauncher.ps1 %1 %2 %3 %4 %5 %6 %7 %8 %9
-exit" | Set-Content $env:localappdata\Microsoft\WindowsApps\CL.bat
-# Launches the batch (powershell needs to specify the .bat but via Run you don't)
+exit" | Set-Content $WindowsAppsDir\CL.bat
+# Launches the batch (powershell needs to specify the .bat but via Run, you don't)
+Write-Output "Launching.."
 Start-Process cl.bat
 exit
-}else{
-Write-Host "Powershell hasn't been detected on your system, please re-enable it in optional features"
-timeout 3
-start-process optionalfeatures
-exit
-}
-
-
 
