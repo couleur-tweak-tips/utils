4cea87ff050cf993a626722eb974a3b7bf7f654f
couleurm <couleur@riseup.net>
2021-09-07 20:04:12 +0200

    lots of fixes

--- GraalVMsetup.ps1
+++ GraalVMsetup.ps1
@@ -1,32 +1,51 @@
 $host.ui.RawUI.WindowTitle = "GraalVM setup for Aetopia's LC Lite, -couleur"
-function DownloadFile{$webClient = [System.Net.WebClient]::new();$webClient.DownloadFile($source, $destination)}
+mode con cols=125 lines=25
+$GraalVer = "21.2.0" #For anyone replacing this with a newer ver: make sure that version's releases got exactly graalvm-ce-java16-windows-amd64, or then update $source as well
 $WR = "$env:LOCALAPPDATA\Microsoft\WindowsApps"
 $MC = "$env:APPDATA\.minecraft"
+$GraalVM = "$env:ProgramData\GraalVM"
+function DownloadGraal{
+Write-Output "Downloading GraalVM (319 MB) this may take a while!"
+$source = "https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-$GraalVer/graalvm-ce-java16-windows-amd64-$GraalVer.zip"
+$destination = "$env:TMP\GraalVM.zip"
+$webClient = [System.Net.WebClient]::new()
+$webClient.DownloadFile($source, $destination)
+Write-Output "Unzipping.."
+Expand-Archive "$destination" "$env:TMP" -Force
+Move-Item "$env:TMP\graalvm-ce-java16-$GraalVer" "$env:ProgramData\GraalVM"
+}
+function InstallLCL{
+Write-Output "Installing LCL..";Start-Process powershell -ArgumentList "irm https://raw.githubusercontent.com/couleur-tweak-tips/utils/main/LCLiteSetup.ps1 | iex" -Wait
+}
 if (-not(test-path $mc)){
 Write-Output "Minecraft's default directory was not found,"
 Write-Output "if you're using a different directory, please indicate it with quotes:"
 $MC = Read-Host "Minecraft Path:"
 }
-$GraalVM = "$env:ProgramData\GraalVM"
-$source = "https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-21.2.0/graalvm-ce-java16-windows-amd64-21.2.0.zip"
-$destination = "$env:TMP\GraalVM.zip"
-if (-not(test-path $destination)){Write-Output "Downloading GraalVM (319 MB) this may take a while!";DownloadFile}
 if (test-path "$GraalVM"){
-    $GraalVMdetection = "1"
-    Write-Output "GraalVM installation detected."
-    Write-Output ""
-    Write-Output "Press D to delete and overwrite"
-    Write-Output "Press S to skip"
-choice /C DS
-if ($LASTEXITCODE -eq "1"){Remove-Item "$GraalVM";mkdir "$GraalVM" Expand-Archive "$destination" "$GraalVM"}
-if ($LASTEXITCODE -eq "2"){}
-}
-if ($null -eq $GraalVMdetection){if (-not(Test-Path "$GraalVM")){mkdir "$GraalVM"}}
-if ($null -eq $GraalVMdetection){Expand-Archive "$destination" "$GraalVM"}
-taskkill /IM LCL.exe /F
-Remove-Item "$WR\LCL.exe" -Force -ErrorAction SilentlyContinue
-if (-not(Test-Path "$WR\LCL.lnk")){Write-Output "Installing LCL..";Start-Process powershell -ArgumentList "irm https://raw.githubusercontent.com/couleur-tweak-tips/utils/main/LCLiteSetup.ps1 | iex" -Wait}
+Write-Output "GraalVM installation detected.
 
+Press D to delete and overwrite
+Press S to skip GraalVM installation
+Press Q to quit"
+choice /C DS
+if ($LASTEXITCODE -eq "1"){ #Delete and overwrite
+Stop-Process -Name LCL -ErrorAction SilentlyContinue
+$AutoHotKey = Get-Process AutoHotKey -ErrorAction SilentlyContinue
+if ($AutoHotKey) {
+Write-Output "AutoHotkey instance running, make sure you close LCL before continuing"
+pause
+}
+Remove-Item "$GraalVM" -Force -Recurse;DownloadGraal}
+if ($LASTEXITCODE -eq "2"){break}
+if ($LASTEXITCODE -eq "3"){exit}
+}else{DownloadGraal}
+Stop-Process -Name LCL -ErrorAction SilentlyContinue
+Remove-Item "$WR\LCL.exe" -Force -ErrorAction SilentlyContinue
+if (Test-Path "$WR\LCL.lnk"){InstallLCL;$LCLinstalled = "installed"}
+if (Test-Path "$WR\LCL.ahk"){InstallLCL;$LCLinstalled = "installed"}
+if (Test-Path "$WR\LCL.exe"){InstallLCL;$LCLinstalled = "installed"}
+if ($null -eq $LCLinstalled){InstallLCL}
 "[LC]
 Version='1.8'
 Arguments=-Xms3G -Xmx3G -XX:+DisableAttachMechanism -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -XX:+EnableJVMCI -XX:+UseJVMCICompiler -XX:+EagerJVMCI -Djvmci.Compiler=graal
