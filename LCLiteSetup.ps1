8059e8692138165a322d71c15ec174361a2469c3
couleurm <couleur@riseup.net>
2021-09-23 22:30:32 +0200

    LCLiteSetup: Logic, uninstall function option, capitals on shortcuts

--- LCLiteSetup.ps1
+++ LCLiteSetup.ps1
@@ -1,39 +1,38 @@
 Write-Host "Which version of LCL do you want?
 
-Press E to get the compiled .EXE version (no need to install AutoHotkey, not modifiable)
-Press A to press the .AHK version (modifiable, needs AutoHotkey installed)
+Press E to get the compiled .EXE version - installs instantly, no need to install AutoHotkey, non modifiable
+Press A to get the native .AHK version - readable and modifiable, needs AutoHotkey installed
 
-Press Q to quit"
+Press U to uninstall LCL"
 $WR = "$env:LOCALAPPDATA\Microsoft\WindowsApps"
 $LCLver = (Invoke-RestMethod -Method GET -Uri "https://api.github.com/repos/Aetopia\Lunar-Client-Lite-Launcher/releases")[0].tag_name
 $Host.UI.RawUI.WindowTitle = "Lunar Client Lite $LCLver installer -couleur"
 $ErrorActionPreference = 'SilentlyContinue'
-CHOICE /C EAQ /N
+function Remove-CurrentLCL{
+    Write-Host 'Removing current installation of LCL..' -ForegroundColor Red
+    Stop-Process -Name LCL
+    Remove-Item "$WR\LCL.exe" -Force
+    Remove-Item "$WR\LCL.ahk" -Force
+    Remove-Item "$WR\config.ini" -Force
+    Remove-Item "$WR\Resources" -Force -Recurse
+    Remove-Item "$WR\LCL.lnk" -Force
+    Remove-Item "$env:appdata\Microsoft\Windows\Start Menu\Programs\Lunar Client Lite.lnk" -Force
+    Remove-Item "$env:appdata\Microsoft\Windows\Start Menu\Programs\Lunar Client Lite.lnk" -Force
+    Remove-Item "$env:appdata\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Lunar Client Lite.lnk" -Force
+}
+
+CHOICE /C EAU /N
 if ($LASTEXITCODE -eq 1){ # ----------------------------------------- .EXE --------------------------------
-        Remove-Item "$WR\LCL.exe" -Force
-        Remove-Item "$WR\LCL.ahk" -Force
-        Remove-Item "$WR\config.ini" -Force
-        Remove-Item "$WR\Resources" -Force -Recurse
-        Remove-Item "$WR\LCL.lnk" -Force
-        Remove-Item "$env:appdata\Microsoft\Windows\Start Menu\Programs\Lunar Client Lite.lnk" -Force
-        Remove-Item "$env:appdata\Microsoft\Windows\Start Menu\Programs\Lunar Client Lite.lnk" -Force
-        Remove-Item "$env:appdata\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Lunar Client Lite.lnk" -Force
-        $source = ((Invoke-RestMethod -Method GET -Uri "https://api.github.com/repos/Aetopia\Lunar-Client-Lite-Launcher/releases")[0].assets | Where-Object name -like LCL.exe ).browser_download_url
-        $destination = "$WR\LCL.exe"
+
+        Remove-CurrentLCL
+        $source = ((Invoke-RestMethod -Method GET -Uri "https://api.github.com/repos/Aetopia\Lunar-Client-Lite-Launcher/releases")[0].assets | Where-Object name -like 'LCL.exe').browser_download_url
         $webClient = [System.Net.WebClient]::new()
-        $webClient.DownloadFile($source, $destination)
+        $webClient.DownloadFile($source, "$WR\LCL.exe")
         $fileExt = '.exe'
     }
     if ($LASTEXITCODE -eq 2){ #----------------------------------------- .AHK -----------------------------------------
-        Stop-Process -Name LCL
-        Remove-Item "$WR\LCL.exe" -Force
-        Remove-Item "$WR\LCL.ahk" -Force
-        Remove-Item "$WR\config.ini" -Force
-        Remove-Item "$WR\Resources" -Force -Recurse
-        Remove-Item "$WR\LCL.lnk" -Force
-        Remove-Item "$env:appdata\Microsoft\Windows\Start Menu\Programs\Lunar Client Lite.lnk" -Force
-        Remove-Item "$env:appdata\Microsoft\Windows\Start Menu\Programs\Lunar Client Lite.lnk" -Force
-        Remove-Item "$env:appdata\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Lunar Client Lite.lnk" -Force
+
+        Remove-CurrentLCL
         $ahkAssoc = cmd /c assoc .ahk
         if (!($ahkAssoc -eq '.ahk=AutoHotkeyScript')){
             if ($null -eq $env:ChocolateyInstall) {
@@ -73,33 +72,37 @@ if ($LASTEXITCODE -eq 1){ # ----------------------------------------- .EXE -----
             $Shortcut.Save()
         }
         
-if ($LASTEXITCODE -eq 3){exit}
+if ($LASTEXITCODE -eq 3){Remove-CurrentLCL;exit}
 
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Lunar Client Lite.lnk")
 if ($fileExt -eq '.ahk'){$Shortcut.IconLocation = "$WR\Resources\Logo.ico"}
 $Shortcut.TargetPath = "$WR\LCL$fileExt"
 $Shortcut.Save()
+Rename-Item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\lunar client lite.lnk" "Lunar Client Lite.lnk"
 
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$env:appdata\Microsoft\Windows\Start Menu\Programs\Lunar Client Lite.lnk")
 if ($fileExt -eq '.ahk'){$Shortcut.IconLocation = "$WR\Resources\Logo.ico"}
 $Shortcut.TargetPath = "$WR\LCL$fileExt"
 $Shortcut.Save()
+Rename-Item "$env:appdata\Microsoft\Windows\Start Menu\Programs\lunar client lite.lnk" "Lunar Client Lite.lnk"
 
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$env:appdata\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Lunar Client Lite.lnk")
 if ($fileExt -eq '.ahk'){$Shortcut.IconLocation = "$WR\Resources\Logo.ico"}
 $Shortcut.TargetPath = "$WR\LCL$fileExt"
 $Shortcut.Save()
+Rename-Item "$env:appdata\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\lunar client lite.lnk" "Lunar Client Lite.lnk"
 
 Write-Host 'Installation finished!' -ForegroundColor Green
-
-start-process powershell -WindowStyle Hidden -ArgumentList "$WR\LCL.lnk"
 ''
 if ($fileExt -eq '.exe'){
-Write-Warning 'If execution fails due to Windows Defender false flagging it, consider getting the .AHK version'
-''
+    Write-Warning 'If execution fails due to Windows Defender false flagging it, consider getting the .AHK version'
+    ''
 }
-Write-Host 'Press any key to exit' -ForegroundColor DarkGray
+Write-Host 'Press any key to launch LCL and exit' -ForegroundColor DarkGray
 While ( -not [Console]::KeyAvailable ) {Start-Sleep -Milliseconds 15}
+if ($fileExt -eq '.exe'){$ExecExt = '.exe'}
+if ($fileExt -eq '.ahk'){$ExecExt = '.lnk'}
+Start-Process powershell -WindowStyle Hidden -ArgumentList "$WR\LCL.$ExecExt"
