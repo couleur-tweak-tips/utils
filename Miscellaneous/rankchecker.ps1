7a3a808092733a20f6a49800e4185547f9e87755
Couleur <82747632+couleurm@users.noreply.github.com>
2022-04-18 18:59:35 +0200

    rankchecker: added MMC & Manacube
    ty bitquence very cute cutie

--- Miscellaneous/rankchecker.ps1
+++ Miscellaneous/rankchecker.ps1
@@ -1,91 +1,139 @@
-function Write-Color {
+function rankchecker {
+[CmdletBinding()]param(
+    [Parameter(ValueFromRemainingArguments = $true)][String]$accargs
+)
 
-    [CmdletBinding()]param(
-        [Parameter(ValueFromRemainingArguments = $true)][String]$Queue
-    )
+    function Write-Color {
+
+        [CmdletBinding()]param(
+            [Parameter(ValueFromRemainingArguments = $true)][String]$Queue
+        )
+        
+        ForEach($Color in [Enum]::GetValues([ConsoleColor])){
+        
+            # Setting up the splatting 
+            Set-Variable -Name $Color -Value @{ForegroundColor = "$Color"} -Force -ErrorAction Inquire
+            Set-Variable -Name "bg$Color" -Value @{BackgroundColor = "$Color"} -Force -ErrorAction Inquire
+        
+        }
+        
+        ForEach($Message in $Queue -Split " '"){
+        
+            if ($Message[0] -ne "'"){$Message = "'" + $Message}
+        
+            Invoke-Expression "Write-Host $Message -NoNewLine"
+           
+        }
+        ''
+        }
     
-    ForEach($Color in [Enum]::GetValues([ConsoleColor])){
-    
-        # Setting up the splatting 
-        Set-Variable -Name $Color -Value @{ForegroundColor = "$Color"} -Force -ErrorAction Inquire
-        Set-Variable -Name "bg$Color" -Value @{BackgroundColor = "$Color"} -Force -ErrorAction Inquire
-    
+    function CheckLunar ($IGN) {
+        Try {
+            $Response = (Invoke-RestMethod https://lunar.gg/u/$IGN -ErrorAction Stop) -Split '\r?\n' 
+        } Catch {
+            Write-Host "`rNone (Parsing failed / Never joined)" -ForegroundColor DarkGray
+            return $null
+        }
+        $Count = 0
+        $Line = ''
+        While ($Line -notlike "*premium-box*"){
+            $Line = $Response[$Count]
+            $Count++
+        }
+        $Rank = $Response[$Count+2].Trim()
+        switch ($Rank){
+            'Owner' {$RankColor = @{ForegroundColor = 'DarkRed'}}
+            'YouTuber' {$RankColor = @{ForegroundColor = 'Red'}}
+            'King' {$RankColor = @{ForegroundColor = 'DarkGreen'}}
+            'Master' {$RankColor = @{ForegroundColor = 'Yellow'}}
+            'Basic' {$RankColor = @{ForegroundColor = 'White'}}
+            'Default' {$RankColor = @{ForegroundColor = 'DarkGray'}}
+        }
+        Write-Host "`r$Rank                 " @RankColor
     }
     
-    ForEach($Message in $Queue -Split " '"){
+    function CheckPlancke ($IGN) {
+        Try {
+            $Response = (Invoke-RestMethod https://plancke.io/hypixel/player/stats/$IGN) -Split '\r?\n'
+        } Catch {
+            Write-Host "`rNone (Parsing failed / Never joined)" -ForegroundColor DarkGray
+            return $null
+        }
     
-        if ($Message[0] -ne "'"){$Message = "'" + $Message}
+        $Line = $Response | Where-Object {$_ -Like "*og:description*"}
+        $Line = $Line -Split '<meta property="og:description" content="'
+        $Line = $Line -Split '" /><meta property='
+        $Title = $Line[5].Trim()
+        switch ($Title){
+            {$_ -Like '`[VIP*`]*'} {$RankColor = @{ForegroundColor = 'Green'}}
+            {$_ -Like '`[MVP*`]*'} {$RankColor = @{ForegroundColor = 'Cyan'}}
+            {$_ -Like '`[YOUTUBE]*'} {$RankColor = @{ForegroundColor = 'Red'}}
+            {$_ -NotLike '`[*`]*'}{$RankColor = @{ForegroundColor = 'DarkGray'};$Rank = 'Default'}
+            Default {$RankColor = @{ForegroundColor = 'DarkGray'}}
+        }
     
-        Invoke-Expression "Write-Host $Message -NoNewLine"
-       
-    }
-    ''
+        Write-Host "`r$Title                 " @RankColor
     }
 
-function CheckLunar ($IGN) {
-    Try {
-        $Response = (Invoke-RestMethod https://lunar.gg/u/$IGN -ErrorAction Stop) -Split '\r?\n' 
-    } Catch {
-        Write-Host "`rNone (Parsing failed / Never joined)" -ForegroundColor DarkGray
-        return $null
-    }
-    $Count = 0
-    $Line = ''
-    While ($Line -notlike "*premium-box*"){
-        $Line = $Response[$Count]
-        $Count++
-    }
-    $Rank = $Response[$Count+2].Trim()
-    switch ($Rank){
-        'Owner' {$RankColor = @{ForegroundColor = 'DarkRed'}}
-        'YouTuber' {$RankColor = @{ForegroundColor = 'Red'}}
-        'King' {$RankColor = @{ForegroundColor = 'DarkGreen'}}
-        'Master' {$RankColor = @{ForegroundColor = 'Yellow'}}
-        'Basic' {$RankColor = @{ForegroundColor = 'White'}}
-        'Default' {$RankColor = @{ForegroundColor = 'DarkGray'}}
-    }
-    Write-Host "`r$Rank                 " @RankColor
-}
+    function CheckMMC ($IGN){
+        Try {
+            $Response = curl.exe -s "https://minemen.club/player/$IGN" -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0'
+        } Catch {
+            Write-Host "`rNone (Parsing failed / Never joined)" -ForegroundColor DarkGray
+            return $null
+        }
+        $Rank = $Response -Split '\r?\n' | Where-Object {$PSItem -Like "*<span class=`"player-info-rank*"}
+        $Rank = ($Rank -Split '">' -Split '</span>')[1]
+        switch ($Rank){
+            'Owner' {$RankColor = @{ForegroundColor = 'DarkRed'}}
+            'Media' {$RankColor = @{ForegroundColor = 'DarkBlue'}}
+            'Partner' {$RankColor = @{ForegroundColor = 'Magenta'}}
+            'Famous' {$RankColor = @{ForegroundColor = 'Orange'}}
+            'Clubber' {$RankColor = @{ForegroundColor = 'DarkGreen'}}
+            'Raver' {$RankColor = @{ForegroundColor = 'Pink'}}
+            'Bartender' {$RankColor = @{ForegroundColor = 'Cyan'}}
+            'Bouncer' {$RankColor = @{ForegroundColor = 'Cyan'}}
+            'Default' {$RankColor = @{ForegroundColor = 'DarkGray'}}
+        }
 
-function CheckPlancke ($IGN) {
-    Try {
-        $Response = (Invoke-RestMethod https://plancke.io/hypixel/player/stats/$IGN) -Split '\r?\n'
-    } Catch {
-        Write-Host "`rNone (Parsing failed / Never joined)" -ForegroundColor DarkGray
-        return $null
+        Write-Host "`r$Rank                 " @RankColor
+
     }
 
-    $Line = $Response | Where-Object {$_ -Like "*og:description*"}
-    $Line = $Line -Split '<meta property="og:description" content="'
-    $Line = $Line -Split '" /><meta property='
-    $Title = $Line[5].Trim()
-    switch ($Title){
-        {$_ -Like '`[VIP*`]*'} {$RankColor = @{ForegroundColor = 'Green'}}
-        {$_ -Like '`[MVP*`]*'} {$RankColor = @{ForegroundColor = 'Cyan'}}
-        {$_ -Like '`[YOUTUBE]*'} {$RankColor = @{ForegroundColor = 'Red'}}
-        {$_ -NotLike '`[*`]*'}{$RankColor = @{ForegroundColor = 'DarkGray'};$Rank = 'Default'}
-        Default {$RankColor = @{ForegroundColor = 'DarkGray'}}
+    function CheckManaCube ($UUID){
+        $UUID = [System.Guid]::Parse($UUID)
+        $Rank = (Invoke-RestMethod https://manacube.com/stats_data/fetch.php?uuid=$UUID).rank
+        $Rank = $Rank.ToUpper()
+        Write-Host "`r$Rank                                   "
     }
-
-    Write-Host "`r$Title                 " @RankColor
-}
-
-if ($args -eq '-'){
-    $Arguments = (Get-Clipboard) -Split '\r?\n'
-}else{
-    $Arguments = $args
-}
-ForEach($Account in $Arguments){
-    if ($All){
-        start https://manacube.com/stats/player/$Account
-        start https://minemen.club/player/$Account
+    
+    if ($accargs -eq '-'){
+        $Arguments = (Get-Clipboard) -Split '\r?\n'
+    }else{
+        $Arguments = $accargs
     }
-    Write-Color "'Results for' @White ':' @DarkGray ' $Account' @Cyan"
-    Write-Host "Checking Hypixel.." -NoNewline
-    CheckPlancke $Account
-    Write-Host "Checking Lunar.." -NoNewline
-    CheckLunar $Account
-    Write-Host https://minemen.club/player/$Account -ForegroundColor DarkGray
-    Write-Host https://manacube.com/stats/player/$Account -ForegroundColor DarkGray
+    ForEach($Account in $Arguments){
 
-}
+        $UUID = (Invoke-RestMethod https://api.mojang.com/user/profile/agent/minecraft/name/$Account).id
+        if (-Not($UUID)){
+            "Error: $Account not found in the Mojang API"
+            continue
+        }
+
+        Write-Color "'Results for' @White ':' @DarkGray ' $Account' @Cyan"
+        Write-Host "Checking Hypixel.." -NoNewline
+        CheckPlancke $Account
+        Write-Host "Checking Lunar.." -NoNewline
+        CheckLunar $Account
+        Write-Host "Checking MMC.." -NoNewline
+        CheckMMC $Account
+        Write-Host "Checking Manacube.." -NoNewline
+        CheckManaCube $UUID
+    
+    }
+@'
+
+Press any key to exit ^_^
+'@
+    $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown') | Out-Null
+} # hello github?
